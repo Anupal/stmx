abe40c2658db9480874a7a5bc729f14e89a4769d
Anupal Mishra <anupal.pm@gmail.com>
2026-01-23 00:53:40 +0000

    allow icon override for cpu subcommand

--- stmx/Commands/CpuCommand.cs
+++ stmx/Commands/CpuCommand.cs
@@ -14,21 +14,32 @@ class CpuCommand : Command
         _systemStats = systemStats ?? throw new ArgumentNullException(nameof(systemStats));
         _icons = icons ?? throw new ArgumentNullException(nameof(icons));
 
-        var showIconOption = new Option<bool>("--icon", ["-i"]);
-        showIconOption.Description = "whether to show icon";
-        showIconOption.DefaultValueFactory = _ => _systemStats.Options.DefaultShowMemoryIcon;
+        var showIconOption = new Option<string>("--icon", ["-i"]);
+        showIconOption.Description = "whether to show icon (optionally provide custom icon)";
+        // this allows users to pass one or more values
+        showIconOption.Arity = ArgumentArity.ZeroOrOne;
         Add(showIconOption);
 
         SetAction(async (parseResult, cancellationToken) =>
         {
-            var showIcon = parseResult.GetValue(showIconOption);
-            await ExecuteAsync(showIcon!);
+            var iconValue = parseResult.GetValue(showIconOption);
+            await ExecuteAsync(
+                parseResult.GetResult(showIconOption) is not null,
+                iconValue!
+            );
         });
     }
 
-    public async Task ExecuteAsync(bool showIcon)
+    public async Task ExecuteAsync(bool showIcon, string iconValue)
     {
-        string cpuIcon = showIcon ? $"{_icons.Options.CpuIcon} " : "";
+        string cpuIcon = "";
+        // if -i was passed
+        if (showIcon)
+        {
+        cpuIcon = string.IsNullOrEmpty(iconValue)
+            ? $"{_icons.Options.CpuIcon} "
+            : $"{iconValue} "; // if a custom icon was also passed
+        }
 
         var cpuPercent = await _systemStats.GetCpuUsagePercent();
         Console.Write($"{cpuIcon}{cpuPercent:F2}%");
