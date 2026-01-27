31926c767d462dc91b6644a28c98821a407cb039
Anupal Mishra <anupal.pm@gmail.com>
2026-01-27 23:19:27 +0000

    Use -p for showing '%'

--- stmx/Commands/CpuCommand.cs
+++ stmx/Commands/CpuCommand.cs
@@ -14,34 +14,49 @@ class CpuCommand : Command
         _systemStats = systemStats ?? throw new ArgumentNullException(nameof(systemStats));
         _icons = icons ?? throw new ArgumentNullException(nameof(icons));
 
-        var showIconOption = new Option<string>("--icon", ["-i"]);
-        showIconOption.Description = "whether to show icon (optionally provide custom icon)";
+        var showIconOption = new Option<string>("--show-icon", ["-i"]);
+        showIconOption.Description = "show icon (optionally provide custom icon)";
         // this allows users to pass one or more values
         showIconOption.Arity = ArgumentArity.ZeroOrOne;
         Add(showIconOption);
 
+        var showPercentIconOption = new Option<string>("--show-percent", ["-p"]);
+        showPercentIconOption.Description = "show percent icon (optionally provide custom icon)";
+        showPercentIconOption.Arity = ArgumentArity.ZeroOrOne;
+        Add(showPercentIconOption);
+
         SetAction(async (parseResult, cancellationToken) =>
         {
             var iconValue = parseResult.GetValue(showIconOption);
+            var percentIconValue = parseResult.GetValue(showPercentIconOption);
             await ExecuteAsync(
                 parseResult.GetResult(showIconOption) is not null,
-                iconValue!
+                iconValue!,
+                parseResult.GetResult(showPercentIconOption) is not null,
+                percentIconValue!
             );
         });
     }
 
-    public async Task ExecuteAsync(bool showIcon, string iconValue)
+    public async Task ExecuteAsync(bool showIcon, string iconValue, bool showPercentIcon, string percentIconValue)
     {
-        string cpuIcon = "";
+        string cpuIcon = "", percentIcon = "";
         // if -i was passed
         if (showIcon)
         {
-        cpuIcon = string.IsNullOrEmpty(iconValue)
-            ? $"{_icons.Options.CpuIcon} "
-            : $"{iconValue} "; // if a custom icon was also passed
+            cpuIcon = string.IsNullOrEmpty(iconValue)
+                ? $"{_icons.Options.CpuIcon} "
+                : $"{iconValue} "; // if a custom icon was also passed
+        }
+
+        if (showPercentIcon)
+        {
+            percentIcon = string.IsNullOrEmpty(percentIconValue)
+                ? _icons.Options.PercentIcon
+                : percentIconValue;
         }
 
         var cpuPercent = await _systemStats.GetCpuUsagePercent();
-        Console.Write($"{cpuIcon}{cpuPercent:F2}%");
+        Console.Write($"{cpuIcon}{cpuPercent:F2}{percentIcon}");
     }
 }
